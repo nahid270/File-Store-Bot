3fa866cb39421cf06312116c0c8106950ca31029
nahid270 <110238003+nahid270@users.noreply.github.com>
2026-01-16 16:22:01 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,7 +17,7 @@ class Config(object):
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002746408715")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003506219023"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
-  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
@@ -36,7 +36,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [CTG](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [CTG](https://telegram.me/TGLinkBase)
  
  I am Super noob Please Support My Hard Work.
 
